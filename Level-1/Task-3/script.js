cb46c7f5606804c25fb717cac8f07556bace2464
Saad Shaikh <shaikhsaad256@gmail.com>
2025-01-19 18:15:16 +0530

    Task-3 Responsive

--- Level-1/Task-3/script.js
+++ Level-1/Task-3/script.js
@@ -1,22 +1,8 @@
-const searchForm = document.getElementById('searchForm');
-const searchInput = document.getElementById('searchInput');
+document.addEventListener('DOMContentLoaded', function () {
+   const hamButton = document.getElementById('hamButton');
+   const nav = document.querySelector('nav');
 
-searchForm.addEventListener('submit', function (event) {
-   event.preventDefault(); 
-   const query = searchInput.value.trim();
-
-   if (query) {
-      const urlPattern = /^(https?:\/\/)?([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,6}(\/[^\s]*)?$/;
-
-      if (urlPattern.test(query)) {
-     
-         window.location.href = query.startsWith('http') ? query : 'https://' + query;
-      } else {
-         
-         const googleSearchUrl = `https://www.google.com/search?q=${encodeURIComponent(query)}`;
-         window.location.href = googleSearchUrl;
-      }
-   } else {
-      alert("Please enter a search term or URL.");
-   }
+   hamButton.addEventListener('click', function () {
+      nav.classList.toggle('menu-active');
+   });
 });
