cb38176800b2155ffb37305bdd00c0b14b4ea146
Saad Shaikh <shaikhsaad256@gmail.com>
2025-01-22 21:35:33 +0530

    Task-5 Completed

--- Level-1/Task-5/script.js
+++ Level-1/Task-5/script.js
@@ -1,42 +1,41 @@
-const moonIcon = document.getElementById('hamIcon');
-const body = document.body;
-const nav = document.querySelector('nav');
-const leftLink = document.getElementById('leftLink');
-const centerLink = document.getElementById('centerLink');
-const rightLink = document.getElementById('rightLink');
-const changeColorButton = document.getElementById('changeColorButton');
+function displayCurrentTime() {
+   const currentTime = new Date();
+   let hours = currentTime.getHours();
+   let minutes = currentTime.getMinutes();
+   let seconds = currentTime.getSeconds();
 
+   hours = hours < 10 ? '0' + hours : hours;
+   minutes = minutes < 10 ? '0' + minutes : minutes;
+   seconds = seconds < 10 ? '0' + seconds : seconds;
 
-let isDarkMode = false;
-
-moonIcon.addEventListener('click', function () {
-   isDarkMode = !isDarkMode;
-
-   if (isDarkMode) {
-      moonIcon.src = 'Assets/Icons/sunIcon.svg';
-      body.style.backgroundColor = '#212121'; 
-      body.style.color = '#ffffff';
-      nav.style.color = '#ffffff';
-      leftLink.style.color = '#ffffff';
-      centerLink.querySelectorAll('a').forEach(link => link.style.color = '#ffffff'); 
-      rightLink.style.color = '#ffffff';
-   } else {
-      moonIcon.src = 'Assets/Icons/moonIcon.svg';
-      body.style.backgroundColor = '#ffffff'; 
-      body.style.color = '#212121'; 
-      nav.style.color = '#212121'; 
-      leftLink.style.color = '#212121'; 
-      centerLink.querySelectorAll('a').forEach(link => link.style.color = '#212121'); 
-      rightLink.style.color = '#212121'; 
-   }
-});
-
-
-function getRandomInt(max) {
-   return Math.floor(Math.random() * max);
+   return `${hours}:${minutes}:${seconds}`;
 }
 
-changeColorButton.addEventListener('click', () => {
-   let randomColor = `rgb(${getRandomInt(255)}, ${getRandomInt(255)}, ${getRandomInt(255)})`;
-   changeColorButton.style.backgroundColor = randomColor;
-});
+function updateTime() {
+   const timeElement = document.getElementById('time');
+   timeElement.textContent = displayCurrentTime();
+}
+
+setInterval(updateTime, 1000);
+
+updateTime();
+
+
+function greetBasedOnTime() {
+   const currentTime = new Date();
+   const currentHour = currentTime.getHours();
+
+   let greetingMessage;
+
+   if (currentHour >= 5 && currentHour < 12) {
+      greetingMessage = "Good Morning!";
+   } else if (currentHour >= 12 && currentHour < 17) {
+      greetingMessage = "Good Afternoon!";
+   } else if (currentHour >= 17 && currentHour < 21) {
+      greetingMessage = "Good Evening!";
+   } else {
+      greetingMessage = "Good Night!";
+   }
+
+   alert(greetingMessage);
+}
